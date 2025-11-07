fc7e84f822af17014938313ba71eb8fec4ff9a1c
unknown <alexbalint26@gmail.com>
2025-11-07 17:50:12 +0200

    Initial commit

---
+++ src/main/java/com/punct/punct_banking/PunctBankingApplication.java
@@ -0,0 +1,13 @@
+package com.punct.punct_banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PunctBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PunctBankingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/punct/punct_banking/PunctBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.punct.punct_banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PunctBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
