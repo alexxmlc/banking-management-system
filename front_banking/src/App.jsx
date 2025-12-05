32dab8f41551564858e057742b505aef82651928
Serban Afrasinei <afrasinei.serban@gmail.com>
2025-12-05 10:56:27 +0200

    atms map merge complete

--- front_banking/src/App.jsx
+++ front_banking/src/App.jsx
@@ -1,22 +1,21 @@
-
-
 import Home from './Home.jsx'
-import { Routes, Route } from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import Login from "./Login.jsx";
 import Register from "./Register.jsx";
+import Atms from "./components/Atms.jsx";
 
-function App(){
-  return(
-    <>
-    <Routes>
-          <Route path = "/" element={<Home/>}/>
-          <Route path = "/login" element={<Login/>}/>
-          <Route path = "/register" element={<Register/>}/>
-          
-    </Routes>
-      
-    </>
-  );
+function App() {
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/login" element={<Login/>}/>
+                <Route path="/register" element={<Register/>}/>
+                <Route path="/map" element={<Atms/>}/>
+            </Routes>
+
+        </>
+    );
 }
 
 export default App
